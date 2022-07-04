02575cf4b227ad44b4ab85caa5e83914f6ef6134
minhoseong <minhos3389@gmail.com>
2022-07-04 19:58:46 +0900

    First letter selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,9 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <details className="selec-none open:text-white open:bg-indigo-400">
-        <summary className="cursor-pointer">what is my fav. food.</summary>
-        <span className="selection:bg-indigo-600 selection:text-white">
-          김치
-        </span>
-      </details>
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400 first-line:text-red-200">
+        Hello everyone!
+      </p>
     </div>
   );
 };
